d5ae66f79c0c4d35f8f6bafacf406ef3ae911627
Richard <Richard.Huang@ufl.edu>
2022-01-29 14:26:11 -0500

    added a WIP castle class

---
+++ castle.py
@@ -0,0 +1,33 @@
+# import pygame
+# import os
+# import fighter
+#
+# #loads image of the Castle
+# #The placeholder is a bigger green dragon.
+# CASTLE = pygame.image.load(os.path.join("images", "gdrag1.png"))
+# CASTLE = pygame.transform.scale(CASTLE, (100, 100))
+#
+# class Castle:
+#     def __init__(self, x, y, hp = 100):
+#         self.x = x
+#         self.y = y
+#         self.img = CASTLE
+#
+# #draws the castle
+#     def draw(self, window):
+#             window.blit(self.img, (self.x, self.y))
+#
+# #castle takes damage
+#     def take_damage(self, attacker):
+#         self.hp -= 1
+#         fighter.remove(attacker)
+#
+#     def get
+#
+#
+#
+
+
+
+
+
